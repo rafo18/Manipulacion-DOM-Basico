89732dcaaf62e732a3d9bff8e8665b55542ee271
Rafael Barreto <rafaelbarreto10@hotmail.com>
2022-08-03 13:12:47 -0400

    Agregado .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,14 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const result = document.querySelector('#result');
+
+//btn.addEventListener('click' , btnOnclick)
+
+//function btnOnclick () {
+//   const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+//    result.innerText = "Resultado "+ sumaInputs;
+//}
+ 
+
